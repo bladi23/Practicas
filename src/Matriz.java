904d2cdac9e5d6e74ee10f8fddbc93f98ab1d6b0
bladi23 <bladivela2003@hotmail.com>
2023-12-21 22:46:22 -0500

    Cambios linea 11, 16

--- src/Matriz.java
+++ src/Matriz.java
@@ -8,11 +8,11 @@ public class Matriz {
         int matriz3x3[][] = new int[3][3];
         for (int f = 0; f < matriz3x3.length; f++) {
             for (int c = 0; c < matriz3x3.length; c++) {
-                System.out.print("Ingrese el numero de la posicion " + "[" + f + "]" + "[ " + c + "]: ");
+                System.out.print("Ingrese el numero en la posicion " + "[" + f + "]" + "[ " + c + "]: ");
                 matriz3x3[f][c] = teclado.nextInt();
             }
         }
-        System.out.println("Mostrar Matriz ingresada");
+        System.out.println("Mostrar Matriz");
         for (int f = 0; f < matriz3x3.length; f++) {
             for (int c = 0; c < matriz3x3.length; c++) {
                 System.out.print(matriz3x3[f][c] + " ");
